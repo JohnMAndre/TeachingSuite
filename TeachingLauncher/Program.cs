488f02617cd631b098571159e6479b3444776370
John <John@JohnMAndre.com>
2015-11-29 19:03:54 +0700

    Added Teaching App Launcher

---
+++ TeachingLauncher/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace TeachingLauncher
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            string strAppPath = Path.GetDirectoryName(Application.ExecutablePath);
+            if (!strAppPath.EndsWith("\\"))
+                strAppPath += "\\";
+            strAppPath += "App\\Teaching.exe";
+            if (File.Exists(strAppPath))
+                Process.Start(strAppPath);
+            else
+                MessageBox.Show("The application is not installed properly. Please re-install it.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
