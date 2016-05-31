e0712fcd1526f41daf3c581e2c9c73b1115f4fdc
John <John@JohnMAndre.com>
2016-06-01 06:37:18 +0700

    Added logging to UpdaterReplacer

--- UpdaterReplacer/Program.cs
+++ UpdaterReplacer/Program.cs
@@ -13,33 +13,36 @@ namespace UpdaterReplacer
             // This will do one very simple task
             // It will accept a source and destination filename, then move the source to the destination (overwriting the destination), then relaunch the destination
             // This should be on a delay to give time for the updater to close down
+            string strLogfile = args[0];
+
             try
             {
                 System.Threading.Thread.Sleep(2500); // 2.5 seconds should be plenty of time for the caller to shut down
 
-                string strSource = args[0];
-                string strDestination = args[1];
+                string strSource = args[1];
+                string strDestination = args[2];
+                string strCallbackParms = args[3];
 
-
-
+                
                 System.Threading.Thread.Sleep(1500);
 
-                System.IO.File.WriteAllText("D:\\output.txt", "Source: " + strSource + " ~~~ Destination: " + strDestination);
+                if (System.IO.File.Exists(strLogfile))
+                    System.IO.File.AppendAllText(strLogfile, "UpdaterReplacer Replaced updater: Source: " + strSource + "; Destination: " + strDestination + Environment.NewLine);
 
                 System.Diagnostics.Process.Start(strDestination);
 
-                System.IO.File.AppendAllText("D:\\output.txt", "Launched: " + strDestination);
-
+                if (System.IO.File.Exists(strLogfile))
+                    System.IO.File.AppendAllText(strLogfile, "UpdaterReplacer Launched: " + strDestination + Environment.NewLine);
+         
             }
             catch (Exception ex)
             {
-                System.IO.File.AppendAllText("D:\\output.txt", "Error: " + ex.Message);
+                if (System.IO.File.Exists(strLogfile))
+                    System.IO.File.AppendAllText(strLogfile, "UpdaterReplacer Error: " + ex.Message + Environment.NewLine);
                 Console.WriteLine("There was an error: " + ex.Message);
                 Console.WriteLine("Press any key to continue.");
                 Console.ReadLine();
             }
-
-            
 
         }
     }
